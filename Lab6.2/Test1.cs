a3188ebed3d348f4aabaa798e80ddc999c3bc02d
Vu Cong Han <vuconghan@example.com>
2025-02-28 16:32:08 +0700

    first commit

---
+++ Lab6.2/Test1.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace Lab6._2
+{
+    [TestClass]
+    public class Test1
+    {
+        private IWebDriver driver;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var options = new ChromeOptions();
+
+            options.AddArgument(@"user-data-dir=C:\Users\Admin\AppData\Local\Google\Chrome\User Data");
+            options.AddArgument("--profile-directory=Profile 5");
+            options.AddArgument("--disable-blink-features=AutomationControlled");
+            options.AddExcludedArgument("enable-automation");
+            options.AddAdditionalOption("useAutomationExtension", false);
+            driver = new ChromeDriver(options);
+            driver.Manage().Window.Maximize();
+        }
+
+        [TestMethod]
+        public void SearchUdemyOnGoogle()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com");
+            IWebElement searchBox = driver.FindElement(By.Name("q"));
+            searchBox.SendKeys("Udemy");
+            searchBox.SendKeys(Keys.Enter);
+            System.Threading.Thread.Sleep(3000);
+            IWebElement udemyLink = driver.FindElement(By.PartialLinkText("Udemy: Online Courses"));
+            udemyLink.Click();
+            System.Threading.Thread.Sleep(5000);
+            Console.WriteLine("Đã truy cập vào: " + driver.Url);
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            driver.Quit();
+
+            Console.WriteLine("Đã kết thúc kiểm thử, đóng trình duyệt.");
+        }
+    }
+}
---
+++ Lab6/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace Lab6
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private IWebDriver driver;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var options = new ChromeOptions();
+
+            options.AddArgument(@"user-data-dir=C:\Users\Admin\AppData\Local\Google\Chrome\User Data");
+            options.AddArgument("--profile-directory=Profile 5");
+            // Khởi tạo trình duyệt Chrome
+            driver = new ChromeDriver(options);
+            // Mở trang Facebook
+            driver.Navigate().GoToUrl("https://www.facebook.com/");
+            // Phóng to cửa sổ trình duyệt
+            driver.Manage().Window.Maximize();
+        }
+
+        [TestMethod]
+        public void TestLogin()
+        {
+            // Tìm và nhập địa chỉ email
+            IWebElement emailField = driver.FindElement(By.Id("email"));
+            emailField.SendKeys("luumytran3001@gmail.com");
+
+            // Tìm và nhập mật khẩu
+            IWebElement passwordField = driver.FindElement(By.Id("pass"));
+            passwordField.SendKeys("TranLuu_5665@iamyoung@");
+
+            // Tìm và nhấn nút đăng nhập
+            IWebElement loginButton = driver.FindElement(By.Name("login"));
+            loginButton.Click();
+
+            // Đợi một chút để kiểm tra xem đăng nhập thành công hay không
+            Thread.Sleep(3000);
+
+            // Kiểm tra xem có đăng nhập thành công không bằng cách kiểm tra URL
+            Assert.IsTrue(driver.Url.Contains("facebook.com")); 
+            Thread.Sleep(10000);
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            // Đóng trình duyệt sau khi test xong
+            driver.Quit();
+        }
+    }
+}
